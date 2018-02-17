5a860949b1223960e7f89cfb29ada5e7055dfc5f
Leon Fedden <lfedd001@gold.ac.uk>
2018-02-17 21:59:01 +0000

    adding search scripts

---
+++ lws_comparison/search_0.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# With LWS mags
+python -W ignore main.py -f 512 -l 1 -g 0 -i 100 -p 1 -m music -c 1
+python -W ignore main.py -f 1024 -l 1 -g 0 -i 100 -p 1 -m music -c 1
+python -W ignore main.py -f 2028 -l 1 -g 0 -i 100 -p 1 -m music -c 1
+python -W ignore main.py -f 4096 -l 1 -g 0 -i 100 -p 1 -m music -c 1
+python -W ignore main.py -f 512 -l 1 -g 0 -i 100 -p 0 -m music -c 1
+python -W ignore main.py -f 1024 -l 1 -g 0 -i 100 -p 0 -m music -c 1
+python -W ignore main.py -f 2028 -l 1 -g 0 -i 100 -p 0 -m music -c 1
+python -W ignore main.py -f 4096 -l 1 -g 0 -i 100 -p 0 -m music -c 1
+python -W ignore main.py -f 512 -l 1 -g 0 -i 100 -p 1 -m speech -c 1
+python -W ignore main.py -f 1024 -l 1 -g 0 -i 100 -p 1 -m speech -c 1
+python -W ignore main.py -f 2028 -l 1 -g 0 -i 100 -p 1 -m speech -c 1
+python -W ignore main.py -f 4096 -l 1 -g 0 -i 100 -p 1 -m speech -c 1
+python -W ignore main.py -f 512 -l 1 -g 0 -i 100 -p 0 -m speech -c 1
+python -W ignore main.py -f 1024 -l 1 -g 0 -i 100 -p 0 -m speech -c 1
+python -W ignore main.py -f 2028 -l 1 -g 0 -i 100 -p 0 -m speech -c 1
+python -W ignore main.py -f 4096 -l 1 -g 0 -i 100 -p 0 -m speech -c 1
+
+python -W ignore main.py -f 512 -l 1 -g 1 -i 100 -p 1 -m music -c 1
+python -W ignore main.py -f 1024 -l 1 -g 1 -i 100 -p 1 -m music -c 1
+python -W ignore main.py -f 2028 -l 1 -g 1 -i 100 -p 1 -m music -c 1
+python -W ignore main.py -f 4096 -l 1 -g 1 -i 100 -p 1 -m music -c 1
+python -W ignore main.py -f 512 -l 1 -g 1 -i 50 -p 1 -m music -c 1
+python -W ignore main.py -f 1024 -l 1 -g 1 -i 50 -p 1 -m music -c 1
+python -W ignore main.py -f 2028 -l 1 -g 1 -i 50 -p 1 -m music -c 1
+python -W ignore main.py -f 4096 -l 1 -g 1 -i 50 -p 1 -m music -c 1
+python -W ignore main.py -f 512 -l 1 -g 1 -i 200 -p 1 -m music -c 1
+python -W ignore main.py -f 1024 -l 1 -g 1 -i 200 -p 1 -m music -c 1
+python -W ignore main.py -f 2028 -l 1 -g 1 -i 200 -p 1 -m music -c 1
+python -W ignore main.py -f 4096 -l 1 -g 1 -i 200 -p 1 -m music -c 1
---
+++ lws_comparison/search_1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# With librosa mags
+python -W ignore main.py -f 512 -l 0 -g 0 -i 100 -p 1 -m music -c 0
+python -W ignore main.py -f 1024 -l 0 -g 0 -i 100 -p 1 -m music -c 0
+python -W ignore main.py -f 2028 -l 0 -g 0 -i 100 -p 1 -m music -c 0
+python -W ignore main.py -f 4096 -l 0 -g 0 -i 100 -p 1 -m music -c 0
+python -W ignore main.py -f 512 -l 0 -g 0 -i 100 -p 0 -m music -c 0
+python -W ignore main.py -f 1024 -l 0 -g 0 -i 100 -p 0 -m music -c 0
+python -W ignore main.py -f 2028 -l 0 -g 0 -i 100 -p 0 -m music -c 0
+python -W ignore main.py -f 4096 -l 0 -g 0 -i 100 -p 0 -m music -c 0
+python -W ignore main.py -f 512 -l 0 -g 0 -i 100 -p 1 -m speech -c 0
+python -W ignore main.py -f 1024 -l 0 -g 0 -i 100 -p 1 -m speech -c 0
+python -W ignore main.py -f 2028 -l 0 -g 0 -i 100 -p 1 -m speech -c 0
+python -W ignore main.py -f 4096 -l 0 -g 0 -i 100 -p 1 -m speech -c 0
+python -W ignore main.py -f 512 -l 0 -g 0 -i 100 -p 0 -m speech -c 0
+python -W ignore main.py -f 1024 -l 0 -g 0 -i 100 -p 0 -m speech -c 0
+python -W ignore main.py -f 2028 -l 0 -g 0 -i 100 -p 0 -m speech -c 0
+python -W ignore main.py -f 4096 -l 0 -g 0 -i 100 -p 0 -m speech -c 0
+
+python -W ignore main.py -f 512 -l 0 -g 1 -i 100 -p 1 -m music -c 0
+python -W ignore main.py -f 1024 -l 0 -g 1 -i 100 -p 1 -m music -c 0
+python -W ignore main.py -f 2028 -l 0 -g 1 -i 100 -p 1 -m music -c 0
+python -W ignore main.py -f 4096 -l 0 -g 1 -i 100 -p 1 -m music -c 0
+python -W ignore main.py -f 512 -l 0 -g 1 -i 50 -p 1 -m music -c 0
+python -W ignore main.py -f 1024 -l 0 -g 1 -i 50 -p 1 -m music -c 0
+python -W ignore main.py -f 2028 -l 0 -g 1 -i 50 -p 1 -m music -c 0
+python -W ignore main.py -f 4096 -l 0 -g 1 -i 50 -p 1 -m music -c 0
+python -W ignore main.py -f 512 -l 0 -g 1 -i 200 -p 1 -m music -c 0
+python -W ignore main.py -f 1024 -l 0 -g 1 -i 200 -p 1 -m music -c 0
+python -W ignore main.py -f 2028 -l 0 -g 1 -i 200 -p 1 -m music -c 0
+python -W ignore main.py -f 4096 -l 0 -g 1 -i 200 -p 1 -m music -c 0
