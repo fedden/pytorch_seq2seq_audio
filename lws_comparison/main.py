f547bc188110380ce7cfa0b12dfbd30459d0f19a
Leon Fedden <lfedd001@gold.ac.uk>
2018-02-17 20:16:48 +0000

    adding args

--- lws_comparison/main.py
+++ lws_comparison/main.py
@@ -1,103 +1,118 @@
 from __future__ import print_function
-import librosa
+
 import torch
 import torch.nn as nn
+import lws
+import librosa
+import argparse
+import numpy as np
 from neural_methods import train, run
 from utils import get_file_name
 from audio import AudioDataset, griffin_lim
 from model import EncoderRNN, AttentionDecoderRNN, to_var
-import numpy as np
-import lws
 
-# Hyper parameters.
-fft_size = 1024
-lws_mags = True
-griffin_lim_phase = True
-griffin_lim_iterations = 100
-perfect_reconstruction = True
-mode = 'music'
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--fft_size', required=True)
+parser.add_argument('-l', '--lws_mags', required=True)
+parser.add_argument('-g', '--griffin_lim_phase', required=True)
+parser.add_argument('-i', '--griffin_lim_iterations', required=True)
+parser.add_argument('-p', '--perfect_reconstruction', required=True)
+parser.add_argument('-m', '--mode', required=True)
+parser.add_argument('-c', '--cuda_device', required=True)
+args = vars(parser.parse_args())
 
+fft_size = int(args['fft_size'])
+lws_mags = bool(args['fft_size'])
+griffin_lim_phase = bool(args['griffin_lim_phase'])
+griffin_lim_iterations = int(args['griffin_lim_iterations'])
+perfect_reconstruction = bool(args['perfect_reconstruction'])
+mode = args['mode']
+cuda_device = int(args['cuda_device'])
 sequence_length = 150
-criterion = nn.MSELoss()
 batch_size = 8
 number_epochs = 20
-
 feature_size = input_size = hidden_size = (fft_size // 2) + 1
 hop_length = fft_size // 4
 path = "./massive_chops/trimmed/vocals_trimmed.wav"
 
-if lws_mags:
-    lws_processor = lws.lws(fft_size,
-                            hop_length,
-                            mode='music',
-                            perfectrec=perfect_reconstruction)
-else:
-    lws_processor = None
+with torch.cuda.device(cuda_device):
 
-# Construct RNNs and optimisers.
-encoder = EncoderRNN(input_size=feature_size,
-                     batch_size=batch_size,
-                     hidden_size=feature_size,
-                     number_layers=3,
-                     dropout=0.0)
+    if lws_mags:
+        lws_processor = lws.lws(fft_size,
+                                hop_length,
+                                mode=mode,
+                                perfectrec=perfect_reconstruction)
+    else:
+        lws_processor = None
 
-decoder = AttentionDecoderRNN('general',
-                              batch_size=batch_size,
-                              hidden_size=hidden_size,
-                              output_size=feature_size,
-                              number_layers=3,
-                              dropout=0.0)
+    # Construct RNNs and optimisers.
+    encoder = EncoderRNN(input_size=feature_size,
+                         batch_size=batch_size,
+                         hidden_size=feature_size,
+                         number_layers=3,
+                         dropout=0.0)
 
-# Enable GPU tensors provided GPUs actually exist!
-if torch.cuda.is_available():
-    encoder.cuda()
-    decoder.cuda()
-    criterion.cuda()
-    torch.cuda.manual_seed(42)
-else:
-    torch.manual_seed(42)
+    decoder = AttentionDecoderRNN('general',
+                                  batch_size=batch_size,
+                                  hidden_size=hidden_size,
+                                  output_size=feature_size,
+                                  number_layers=3,
+                                  dropout=0.0)
 
-encoder_optimiser = torch.optim.Adam(encoder.parameters(), lr=0.00001)
-decoder_optimiser = torch.optim.Adam(decoder.parameters(), lr=0.00001 * 5.0)
+    criterion = nn.MSELoss()
 
-dataset = AudioDataset(fft_size=fft_size,
-                       hop_length=hop_length,
-                       batch_size=batch_size,
-                       lws_processor=lws_processor,
-                       sequence_length=sequence_length,
-                       path=path)
+    # Enable GPU tensors provided GPUs actually exist!
+    if torch.cuda.is_available():
+        encoder.cuda()
+        decoder.cuda()
+        criterion.cuda()
+        torch.cuda.manual_seed(42)
+    else:
+        torch.manual_seed(42)
 
-# Train.
-previous_epoch = -1
-for x, y, epoch in dataset.get_next_batch(number_epochs):
+    encoder_optimiser = torch.optim.Adam(encoder.parameters(), lr=0.00001)
+    decoder_optimiser = torch.optim.Adam(decoder.parameters(), lr=0.00005)
 
-    loss = train(encoder, decoder, encoder_optimiser, decoder_optimiser,
-                 x, y, criterion, 5.0)
-    print("epoch {}/{}, loss {}".format(epoch, number_epochs, loss), end="\r")
+    dataset = AudioDataset(fft_size=fft_size,
+                           hop_length=hop_length,
+                           batch_size=batch_size,
+                           lws_processor=lws_processor,
+                           sequence_length=sequence_length,
+                           path=path)
 
-    if epoch != previous_epoch:
-        previous_epoch = epoch
-        print('')
+    # Train.
+    previous_epoch = -1
+    epoch_str = "epoch {}/{}, loss {}"
+    for x, y, epoch in dataset.get_next_batch(number_epochs):
 
-# Inference.
-indices = np.random.permutation(dataset.dataset_size)[:batch_size]
-start_sequences = to_var(torch.from_numpy(dataset.x[indices]))
-predicted_batch = run(encoder, decoder, start_sequences, 20)
-first_magnitudes = predicted_batch[0]
+        loss = train(encoder, decoder, encoder_optimiser, decoder_optimiser,
+                     x, y, criterion, 5.0)
+        print(epoch_str.format(epoch, number_epochs, loss), end="\r")
 
-# Reconstruct phase and generate samples.
-if griffin_lim_phase:
-    predicted_audio = griffin_lim(first_magnitudes,
-                                  n_iter=griffin_lim_iterations,
-                                  window='hann',
-                                  n_fft=fft_size,
-                                  hop_length=hop_length)
-else:
-    predicted_stfts = lws_processor.run_lws(first_magnitudes)
-    predicted_audio = lws_processor.istft(predicted_stfts)
+        if epoch != previous_epoch:
+            previous_epoch = epoch
+            print('')
 
-# Get file name and write to wav.
-file_name = get_file_name(fft_size, lws_mags, griffin_lim_phase,
-                          griffin_lim_iterations, perfect_reconstruction, mode)
-librosa.output.write_wav(file_name, predicted_audio, dataset.sample_rate)
-print("Done.")
+    # Inference.
+    indices = np.random.permutation(dataset.dataset_size)[:batch_size]
+    start_sequences = to_var(torch.from_numpy(dataset.x[indices]))
+    predicted_batch = run(encoder, decoder, start_sequences, 20)
+    first_magnitudes = predicted_batch[0]
+
+    # Reconstruct phase and generate samples.
+    if griffin_lim_phase:
+        predicted_audio = griffin_lim(first_magnitudes,
+                                      n_iter=griffin_lim_iterations,
+                                      window='hann',
+                                      n_fft=fft_size,
+                                      hop_length=hop_length)
+    else:
+        predicted_stfts = lws_processor.run_lws(first_magnitudes)
+        predicted_audio = lws_processor.istft(predicted_stfts)
+
+    # Get file name and write to wav.
+    file_name = get_file_name(fft_size, lws_mags, griffin_lim_phase,
+                              griffin_lim_iterations, perfect_reconstruction,
+                              mode)
+    librosa.output.write_wav(file_name, predicted_audio, dataset.sample_rate)
+    print("Done.")
